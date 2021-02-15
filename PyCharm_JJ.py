23252e27e3295ae1a4a9c3cb915f5b1d7e0b1b46
bizbbiz <bizbbiz@yahoo.ca>
2021-02-15 14:28:01 -0500

    Create PyCharm_JJ.py

---
+++ PyCharm_JJ.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import os
+import tensorflow as tf
+import cv2
+from tensorflow import keras
+from  matplotlib import pyplot as plt
+import matplotlib.image as mpimg
+
+import keras as k
+from matplotlib import pyplot
+from keras.preprocessing.image import ImageDataGenerator
+from google.colab import drive
+drive.mount('/content/drive')
